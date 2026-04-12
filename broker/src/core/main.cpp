ccf4593b62acddba3e4dd42665f8b4d1872a8645
Jangmyun <cndlsrb2739@gmail.com>
2026-04-12 17:49:56 +0900

    [feat]: broker (cpp project) init

---
+++ broker/src/core/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include "connection_table.h"
+
+int main() {
+    printf("Core broker starting...\n");
+    // TODO: initialize ConnectionTable, connect mosquitto, register LWT
+    return 0;
+}
---
+++ broker/src/edge/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include "connection_table.h"
+
+int main() {
+    printf("Edge broker starting...\n");
+    // TODO: connect to Core, receive ConnectionTable, measure RTT to peers
+    return 0;
+}
