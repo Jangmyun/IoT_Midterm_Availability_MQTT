ee9010da86212362c1ebf2288448863d1ebe8653
Jangmyun <cndlsrb2739@gmail.com>
2026-04-15 16:06:57 +0900

    [feat] 사용할 topic, global context, signal

--- broker/src/edge/main.cpp
+++ broker/src/edge/main.cpp
@@ -1,8 +1,36 @@
 #include <cstdio>
-#include "connection_table.h"
+#include <cstring>
+#include <cstdlib>
+#include <csignal>
+#include <ctime>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <mosquitto.h>
+#include "connection_table_manager.h"
+#include "mqtt_json.h"
+#include "message.h"
+#include "uuid.h"
 
-int main() {
-    printf("Edge broker starting...\n");
-    // TODO: connect to Core, receive ConnectionTable, measure RTT to peers
+// constant TOPICs =====================================================
+#define TOPIC_TOPOLOGY         "campus/monitor/topology"
+#define TOPIC_CORE_WILL_ALL    "campus/will/core/#"
+#define TOPIC_LWT_NODE_PREFIX  "campus/will/node/"
+#define TOPIC_STATUS_PREFIX    "campus/monitor/status/"
+#define TOPIC_PING_PREFIX      "campus/monitor/ping/"
+#define TOPIC_PONG_PREFIX      "campus/monitor/pong/"
+
+// Global State =====================================================
+static volatile bool g_running = true;
+
+struct EdgeContext {
+    char                    edge_id[UUID_LEN];
+    char                    node_ip[IP_LEN];
+    uint16_t                node_port;
+    ConnectionTableManager* ct_manager;
+};
+
+static void handle_signal(int) { g_running = false; }
     return 0;
 }
