c9156405a0560f0c43646c2b1c278fa1eec50743
Jangmyun <cndlsrb2739@gmail.com>
2026-04-15 11:49:48 +0900

    [feat] MQTT Topic 문자열 define

--- broker/src/core/main.cpp
+++ broker/src/core/main.cpp
@@ -1,8 +1,22 @@
 #include <cstdio>
-#include "connection_table.h"
+#include <cstring>
+#include <cstdlib>
+#include <csignal>
+#include <ctime>
+#include <mosquitto.h>
+#include "connection_table_manager.h"
+#include "mqtt_json.h"
+#include "message.h"
 
-int main() {
-    printf("Core broker starting...\n");
-    // TODO: initialize ConnectionTable, connect mosquitto, register LWT
+// constant TOPICs 
+#define TOPIC_LWT_CORE_PREFIX  "campus/will/core/"
+#define TOPIC_TOPOLOGY         "campus/monitor/topology"
+#define TOPIC_DATA_ALL         "campus/data/#"
+#define TOPIC_RELAY_ALL        "campus/relay/#"
+#define TOPIC_NODE_STATUS_ALL  "campus/monitor/status/#"
+#define TOPIC_NODE_WILL_ALL    "campus/will/node/#"
+#define TOPIC_CORE_WILL_ALL    "campus/will/core/#"
+#define TOPIC_CT_SYNC          "_core/sync/connection_table"
+#define TOPIC_ELECTION_ALL     "_core/election/#"
     return 0;
 }
